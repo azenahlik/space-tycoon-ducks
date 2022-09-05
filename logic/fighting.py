0eea000fb729d3365567322cd0c7677669a2f811
azenahlik <ales.z@hotmail.com>
2022-09-05 02:30:27 +0200

    Better fighting

--- logic/fighting.py
+++ logic/fighting.py
@@ -1,8 +1,11 @@
 from typing import Dict
 
+from space_tycoon_client.models.move_command import MoveCommand
 from space_tycoon_client.models.attack_command import AttackCommand
 from space_tycoon_client.models.data import Data
 from space_tycoon_client.models.ship import Ship
+from space_tycoon_client.models.destination import Destination
+# from space_tycoon_client.models.target import Target
 
 from utils.general import countDistanceShips
 
@@ -12,12 +15,26 @@ def get_fighting_commands(data: Data, player_id: str) -> dict:
 
     my_attack_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
                                    data.ships.items() if ship.player == player_id and ship.ship_class in ["1","4","5"]}
+    my_attack_ships_without_ms: Dict[Ship] = {ship_id: ship for ship_id, ship in
+                                   data.ships.items() if ship.player == player_id and ship.ship_class in ["4","5"]}
+    ms = [ship_id for ship_id, ship in my_attack_ships.items() if ship.ship_class == "1"]
     chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                      data.ships.items() if ship.player != player_id and ship.ship_class in ["1","4","5"]}
+                                      data.ships.items() if ship.player != player_id and ship.ship_class in ["4","5"]}
+    if chosen_enemy_ships == {}:
+        chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
+                                          data.ships.items() if
+                                          ship.player != player_id and ship.ship_class in ["1"]}
     if chosen_enemy_ships == {}:
         chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
                                           data.ships.items() if
                                           ship.player != player_id}
+    if chosen_enemy_ships == {}:
+        # Nothing to do now
+        if ms:
+            for attack_ship in my_attack_ships_without_ms:
+                commands[attack_ship] = MoveCommand(Destination(target=ms[0]))
+        else:
+            print("No MS to use")
     distances = [(x, countDistanceShips(list(my_attack_ships.values())[0], chosen_enemy_ships[x])) for x in chosen_enemy_ships]
     distances_by_shortest = sorted(distances, key=lambda d: d[1])
     if len(chosen_enemy_ships):
