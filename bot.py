dc8ccface1c246b2cca2aa047c1fb1487423db56
Ales Zenahlik <azenahlik@deloittece.com>
2022-09-05 04:29:39 +0200

    Added combat repair

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ from space_tycoon_client.models.ship import Ship
 from space_tycoon_client.models.static_data import StaticData
 from space_tycoon_client.rest import ApiException
 from logic.fighting import get_fighting_commands
+from logic.fighting import get_repair_commands
 from logic.construction import get_construction_commands
 from logic.trading import getTrandingOptions
 
@@ -109,6 +110,8 @@ class Game:
         # Attack Commands
         attack_commands = get_fighting_commands(self.data, self.player_id)
         commands.update(attack_commands)
+        fixing_commands = get_repair_commands(self.data, self.player_id)
+        commands.update(fixing_commands)
 
         # Construction Commands
         construction_commands = get_construction_commands(self.data, self.player_id)
--- logic/fighting.py
+++ logic/fighting.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 from space_tycoon_client.models.move_command import MoveCommand
 from space_tycoon_client.models.attack_command import AttackCommand
+from space_tycoon_client.models.repair_command import RepairCommand
 from space_tycoon_client.models.data import Data
 from space_tycoon_client.models.ship import Ship
 from space_tycoon_client.models.destination import Destination
@@ -34,5 +35,18 @@ def get_fighting_commands(data: Data, player_id: str) -> dict:
         for attack_ship in my_attack_ships:
             commands[attack_ship] = AttackCommand(distances_by_shortest[0][0])
             print(f'Fighter {attack_ship} attacking enemy fighter ID {list(chosen_enemy_ships.keys())[0]}')
+
+    return commands
+
+
+def get_repair_commands(data: Data, player_id: str) -> dict:
+    commands = {}
+
+    my_attack_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
+                                   data.ships.items() if ship.player == player_id and ship.ship_class in ["1","4","5"]}
+
+    for attack_ship in my_attack_ships:
+        if my_attack_ships[attack_ship].life < 50:
+            commands[attack_ship] = RepairCommand()
 
     return commands
