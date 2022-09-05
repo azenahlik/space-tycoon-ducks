aac49cbcc7856de0c3790650a8408db07e068dd8
husakja <husak.h@seznam.cz>
2022-09-05 04:35:53 +0200

    Ranges

--- logic/trading.py
+++ logic/trading.py
@@ -72,19 +72,28 @@ def getResourcesRanges(data: Data):
     planets = data.planets
     resources = ["1","10","11","12","13","14","15","16","17","18","19","2","20","21","22","23","3","4","5","6","7","8","9"]
     ranges = {}
+
+    # prepare dict
     for i in resources:
         ranges[i] = {
+            'from': None,
+            'to': None,
             'sell': 0,
             'buy': 99999999,
             'diff': 0,
         }
 
-    for i in self.data.planets.keys():
+    # find ranges
+    for i in data.planets.keys():
         for j in planets[i].resources:
             if (planets[i].resources[j].buy_price != None and ranges[j]['buy'] > planets[i].resources[j].buy_price):
                 ranges[j]['buy'] = planets[i].resources[j].buy_price
+                ranges[j]['to'] = i
             if (planets[i].resources[j].sell_price != None and ranges[j]['sell'] < planets[i].resources[j].sell_price):
                 ranges[j]['sell'] = planets[i].resources[j].sell_price
+                ranges[j]['from'] = i
+  
+    # compute diff
     for i in ranges:
         ranges[i]['diff'] = ranges[i]['sell'] - ranges[i]['buy']
 
