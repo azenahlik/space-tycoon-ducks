32b75a14cd45bad3172c154f4e5c643bbc35c6b0
Ales Zenahlik <azenahlik@deloittece.com>
2022-09-05 13:20:13 +0200

    Prefer fighters

--- utils/general.py
+++ utils/general.py
@@ -4,4 +4,7 @@ from space_tycoon_client.models.ship import Ship
 
 
 def countDistanceShips(ship1: Ship, ship2: Ship):
-    return sqrt((ship1.position[0] - ship2.position[0])**2+(ship1.position[1] - ship2.position[1])**2)
+    extra = 0  # Hack to prefer fighters over motherships
+    if "ship_class" in ship2.__dict__.keys() and ship2.ship_class == "1":
+        extra = 10
+    return sqrt((ship1.position[0] - ship2.position[0])**2+(ship1.position[1] - ship2.position[1])**2) + extra
