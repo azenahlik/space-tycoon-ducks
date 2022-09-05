a75455c582d337b51d926469a921d87c931d331e
Ales Zenahlik <azenahlik@deloittece.com>
2022-09-05 22:00:51 +0200

    Refactored fighting

--- logic/fighting.py
+++ logic/fighting.py
@@ -10,6 +10,8 @@ from space_tycoon_client.models.destination import Destination
 import logging
 
 from utils.general import countDistanceShips
+from utils.ship_helpers import get_my_attack_ships, get_enemy_attack_ships,\
+    get_mothership, get_distance_ships_ms_extra, get_enemy_ships, get_closest_ships
 
 
 logger = logging.getLogger(__name__)
@@ -18,65 +20,54 @@ logger = logging.getLogger(__name__)
 def get_ms_fighting_commands(data: Data, player_id: str) -> dict:
     commands = {}
 
-    my_attack_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                   data.ships.items() if ship.player == player_id and ship.ship_class in ["1","4","5"]}
+    my_attack_ships: Dict[Ship] = get_my_attack_ships(data, player_id)
     ms = [ship_id for ship_id, ship in my_attack_ships.items() if ship.ship_class == "1"]
-
+    mothership = get_mothership(data, player_id)
     if not ms:
         return commands
 
-    chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                      data.ships.items() if ship.player != player_id and ship.ship_class in ["1","4","5"]}
+    chosen_enemy_ships: Dict[Ship] = get_enemy_attack_ships(data, player_id)
     no_enemy_attack_ships = False
     if chosen_enemy_ships == {}:
-        chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                          data.ships.items() if
-                                          ship.player != player_id}
+        chosen_enemy_ships: Dict[Ship] = get_enemy_ships(data, player_id)
         no_enemy_attack_ships = True
-    distances = [(x, countDistanceShips(list(my_attack_ships.values())[0], chosen_enemy_ships[x])) for x in chosen_enemy_ships]
-    distances_by_shortest = sorted(distances, key=lambda d: d[1])
+    closest_filtered_enemy_ships = get_closest_ships(data, player_id, mothership, chosen_enemy_ships)
     player_data = [data.players[x] for x in data.players if x == player_id][0]
 
     if len(chosen_enemy_ships):
-        if player_data.net_worth.money > 300000 or distances_by_shortest[0][1] < 200 or no_enemy_attack_ships:
-            commands[ms[0]] = AttackCommand(distances_by_shortest[0][0])
-            logger.info(f'MS attacking enemy fighter ID {list(chosen_enemy_ships.keys())[0]}')
+        if player_data.net_worth.money > 300000 or list(closest_filtered_enemy_ships[0].values())[0] < 200 or no_enemy_attack_ships:
+            commands[ms[0]] = AttackCommand(list(closest_filtered_enemy_ships[0].keys())[0])
+            logger.info(f'MS ({list(mothership.values())[0].name}) attacking enemy ship ID {list(chosen_enemy_ships.keys())[0]}')
 
     return commands
 
 
 def get_fighter_fighting_commands(data: Data, player_id: str, aggro_distance: int) -> dict:
     commands = {}
-    my_attack_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                   data.ships.items() if ship.player == player_id and ship.ship_class in ["1","4","5"]}
-    my_attack_ships_without_ms: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                   data.ships.items() if ship.player == player_id and ship.ship_class in ["4","5"]}
+    my_attack_ships: Dict[Ship] = get_my_attack_ships(data, player_id)
+    my_attack_ships_without_ms: Dict[Ship] = get_my_attack_ships(data, player_id, False)
+
     if not my_attack_ships_without_ms:
         return commands
     ms = [ship_id for ship_id, ship in my_attack_ships.items() if ship.ship_class == "1"]
-    chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                      data.ships.items() if ship.player != player_id and ship.ship_class in ["4","5"]}
+    mothership = get_mothership(data, player_id)
+    chosen_enemy_ships: Dict[Ship] = get_enemy_attack_ships(data, player_id, False)
     if chosen_enemy_ships == {}:
-        chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                          data.ships.items() if
-                                          ship.player != player_id and ship.ship_class in ["1"]}
+        chosen_enemy_ships: Dict[Ship] = get_enemy_attack_ships(data, player_id)
     no_enemy_attack_ships = False
     if chosen_enemy_ships == {}:
-        chosen_enemy_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                          data.ships.items() if
-                                          ship.player != player_id}
+        chosen_enemy_ships: Dict[Ship] = get_enemy_ships(data, player_id)
         no_enemy_attack_ships = True
-    distances = [(x, countDistanceShips(list(my_attack_ships_without_ms.values())[0], chosen_enemy_ships[x])) for x in chosen_enemy_ships]
-    distances_by_shortest = sorted(distances, key=lambda d: d[1])
+    closest_filtered_enemy_ships = get_closest_ships(data, player_id, mothership, chosen_enemy_ships)
 
     if len(chosen_enemy_ships):
-        if ms and min([x[1] for x in distances_by_shortest]) > aggro_distance and not no_enemy_attack_ships:
+        if ms and min([list(x.values())[0] for x in closest_filtered_enemy_ships]) > aggro_distance and not no_enemy_attack_ships:
             for attack_ship in my_attack_ships_without_ms:
                 commands[attack_ship] = MoveCommand(destination=Destination(target=ms[0]))
                 logger.info(f'Fighter {attack_ship} is staying with MS')
         else:
             for attack_ship in my_attack_ships_without_ms:
-                commands[attack_ship] = AttackCommand(distances_by_shortest[0][0])
+                commands[attack_ship] = AttackCommand(list(closest_filtered_enemy_ships[0].values())[0])
                 logger.info(f'Fighter {attack_ship} attacking enemy fighter ID {list(chosen_enemy_ships.keys())[0]}')
 
     return commands
@@ -94,7 +85,7 @@ def get_repair_commands(data: Data, player_id: str) -> dict:
         return commands
 
     for attack_ship in my_attack_ships:
-        if my_attack_ships[attack_ship].life < 150:
+        if my_attack_ships[attack_ship].life < 190:
             try:
                 logger.info(f"Healing attack ship {my_attack_ships[attack_ship].name}")
             except:
---
+++ utils/ship_helpers.py
@@ -0,0 +1,53 @@
+from typing import Dict
+from math import sqrt
+from space_tycoon_client.models.move_command import MoveCommand
+from space_tycoon_client.models.attack_command import AttackCommand
+from space_tycoon_client.models.repair_command import RepairCommand
+from space_tycoon_client.models.data import Data
+from space_tycoon_client.models.ship import Ship
+from space_tycoon_client.models.destination import Destination
+# from space_tycoon_client.models.target import Target
+from utils.general import countDistanceShips
+
+
+def get_distance_ships_ms_extra(ship1: Ship, ship2: Ship):
+    """Extra distance to motherships to dissuade squads from prioritizing them in firefights"""
+    extra = 0  # Hack to prefer fighters over motherships
+    if "ship_class" in ship2.__dict__.keys() and ship2.ship_class == "1":
+        extra = 10
+    return sqrt((ship1.position[0] - ship2.position[0])**2+(ship1.position[1] - ship2.position[1])**2) + extra
+
+
+def get_my_attack_ships(data: Data, player_id: str, include_ms: bool = True) -> Dict:
+    return {ship_id: ship for ship_id, ship in data.ships.items() if
+            ship.player == player_id and ship.ship_class in ["1" if include_ms else "X", "4", "5"]}
+
+
+def get_enemy_attack_ships(data: Data, player_id: str, include_ms: bool = True) -> Dict:
+    return {ship_id: ship for ship_id, ship in data.ships.items() if
+            ship.player != player_id and ship.ship_class in ["1" if include_ms else "X", "4", "5"]}
+
+
+def get_enemy_ships(data: Data, player_id: str) -> Dict:
+    return {ship_id: ship for ship_id, ship in data.ships.items() if
+            ship.player != player_id}
+
+
+def get_mothership(data: Data, player_id: str) -> Dict:
+    return {ship_id: ship for ship_id, ship in data.ships.items() if
+            ship.player == player_id and ship.ship_class in ["1"]}
+
+
+def get_distance_ships(ship1: Ship, ship2: Ship):
+    extra = 0  # Hack to prefer fighters over motherships
+    if "ship_class" in ship2.__dict__.keys() and ship2.ship_class == "1":
+        extra = 10
+    return sqrt((ship1.position[0] - ship2.position[0])**2+(ship1.position[1] - ship2.position[1])**2) + extra
+
+
+def get_closest_ships(data: Data, player_id: str, my_ship: Ship, enemy_ship_dict: dict) -> list:
+    distances = [{x: get_distance_ships(list(my_ship.values())[0], enemy_ship_dict[x])} for x in
+                 enemy_ship_dict]
+    distances_by_shortest = sorted(distances, key=lambda d: list(d.values())[0])
+    return distances_by_shortest
+
